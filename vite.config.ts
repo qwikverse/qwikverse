50970fe53e791ba92272035b93fe146e43261d88
John Reemar Dela Rosa <jrddelarosa@gmail.com>
2022-12-11 10:51:17 +0800

    chore: added polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import { imagetools } from "vite-imagetools";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+import rollupNodePolyFill from "rollup-plugin-node-polyfills";
+import builtins from "rollup-plugin-node-builtins";
 
 const path = require("path");
 
@@ -17,6 +21,30 @@ export default defineConfig(() => {
     resolve: {
       alias: {
         "~": path.resolve(__dirname, "./src"),
+      },
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        target: "es2020",
+        supported: { bigint: true },
+        plugins: [
+          NodeGlobalsPolyfillPlugin({
+            process: true,
+            buffer: true,
+          }),
+          NodeModulesPolyfillPlugin(),
+        ],
+      },
+    },
+    build: {
+      target: "es2020",
+      rollupOptions: {
+        plugins: [
+          // Enable rollup polyfills plugin
+          // used during production bundling
+          builtins(),
+          rollupNodePolyFill(),
+        ],
       },
     },
   };
