b2b5dad1d50245378257c43083071b92895e5710
John Reemar Dela Rosa <jrddelarosa@gmail.com>
2022-12-11 21:36:01 +0800

    update homepage loader

--- src/components/welcome/index.tsx
+++ src/components/welcome/index.tsx
@@ -5,20 +5,23 @@ import { QwikLottie } from "qwik-lottie";
 export default component$(() => {
     const astronaut = useStore({
         options: {
-        path: "https://assets7.lottiefiles.com/packages/lf20_5gkEN24YTZ.json",
+        path: "https://assets3.lottiefiles.com/packages/lf20_Ns4TLz.json",
         },
     });
-    // const loading = useStore({
-    //     options: {
-    //     path: "https://assets4.lottiefiles.com/datafiles/aba45c7b75d547282b2dbdc97969412b/progress_bar.json",
-    //     },
-    // });
+    const loading = useStore({
+        options: {
+        path: "https://assets2.lottiefiles.com/private_files/lf30_l8csvun7.json",
+        },
+    });
 
   return (
     <div class="w-screen h-screen grid justify-items-center place-content-center">
         <div class=" w-6/12">
           <QwikLottie options={astronaut.options} />
-          
+          <div class="w-auto h-auto -mt-2">
+            <QwikLottie options={loading.options} />
+          </div>
+          <p class="text-center">Creation in progress...</p>
       </div>
     </div>
   );
